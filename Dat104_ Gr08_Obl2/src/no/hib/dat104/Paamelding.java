1dab07b2b0b0dc80120056986678911e8d18c55a
96tormat <96tormat@158-37-230-131.hib.no>
2016-09-26 11:29:01 +0200

    Forside med jsper funker

---
+++ Dat104_ Gr08_Obl2/src/no/hib/dat104/Paamelding.java
@@ -0,0 +1,24 @@
+package no.hib.dat104;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Paamelding")
+public class Paamelding extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("WEB-INF/jsp/paameldingsskjema.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	}
+
+}
