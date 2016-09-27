04ab3d8726b235465aad163d753e80ae728da294
Torstein <Torstein@158.37.233.99>
2016-09-27 10:56:27 +0200

    konflikt fix

--- Dat104_ Gr08_Obl2/test/no/hib/dat104/model/DeltagerEAOtest.java
+++ Dat104_ Gr08_Obl2/test/no/hib/dat104/model/DeltagerEAOtest.java
@@ -1,22 +1,56 @@
 package no.hib.dat104.model;
 
-import org.junit.Before;
-import org.junit.Test;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class DeltagerEAOtest {
-	
-	DeltagerEAO deao;
-	Deltager d0;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-	@Before
-	public void init() {
-		deao = new DeltagerEAO();
-		d0 = new Deltager(55556666,"Per", "Fjes", "M");
-	}
+/**
+ * Servlet implementation class DeltagerEAOtest
+ */
+@WebServlet("/DeltagerEAOtest")
+public class DeltagerEAOtest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	
-	@Test
-	public void leggTil() {
+	
+	@EJB
+	private DeltagerEAO deao;
+	
+	private Deltager d0 = new Deltager(55550000, "Test", "Testsen", "M");
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out= response.getWriter();
 		
+		
+		//legg til og fjern
+		deao.leggTilEllerOppdater(d0);
+		out.println("d0 eksisterer:" + deao.deltagerEksisterer(d0) + "<br>");
+		deao.slettDeltager(d0.getMobil());
+		out.println("d0 eksisterer ikke lengre:" + !deao.deltagerEksisterer(d0) + "<br>");
+		
+		//oppdater betaling
+		deao.leggTilEllerOppdater(d0);
+		out.println("d0 har betalt:" + deao.finnDeltager(d0.getMobil()).isBetalingsstatus() + "<br>");
+		deao.oppdaterBetalingsstatus(d0.getMobil(), true);
+		out.println("d0 har betalt:" + deao.finnDeltager(d0.getMobil()).isBetalingsstatus() + "<br>");
+		
+		
+		
+		
+		deao.slettDeltager(d0.getMobil());
+		
+		
+
 	}
+
+
+
 
 }
